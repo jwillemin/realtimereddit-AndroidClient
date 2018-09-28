969a1b90cdfce93056b52d800c49cc2cc772e6f8
jwillemin <jessica.willemin@gmail.com>
2018-09-28 15:08:32 -0400

    generate initial android project files

---
+++ app/src/main/java/com/example/jessi/realtimereddit_androidclient/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jessi.realtimereddit_androidclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
