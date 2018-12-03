dc836c5554412b063e10595db41616ef13dd17e7
jwillemin <jessica.willemin@gmail.com>
2018-12-02 21:57:28 -0500

    add action items to nav bar

--- app/src/main/java/com/example/jessi/realtimereddit_androidclient/MainActivity.java
+++ app/src/main/java/com/example/jessi/realtimereddit_androidclient/MainActivity.java
@@ -4,8 +4,9 @@ import android.content.Intent;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -31,6 +32,30 @@ public class MainActivity extends AppCompatActivity implements GetPosts.AsyncRes
                 fetchData();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.nav_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.real_time_toggle:
+                return true;
+
+            case R.id.subreddit:
+                Intent intent = new Intent(MainActivity.this, SubredditSelectionActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     public void fetchData(){
